81ad666005f0532712562642011179234a445adb
Mark Sears <mjbmon@swcp.com>
2024-09-16 13:33:59 -0600

    Modified configure, configure.bat to search list of directories.

--- mps-buildtools/scripts/configure.bat
+++ mps-buildtools/scripts/configure.bat
@@ -13,25 +13,25 @@ set P="%PYTHON% --version"
 FOR /F "delims=" %%i IN ('%P%') DO set PYTHON_VERSION=%%i
 echo "PYTHON VERSION is %PYTHON_VERSION%"
 
-echo "Getting MPS_BUILDTOOLS"
-if exist %PROJECT_TOP_DIR%\mps-buildtools (
-  echo set MPS_BUILDTOOLS=%PROJECT_TOP_DIR%\mps-buildtools
-  set MPS_BUILDTOOLS=%PROJECT_TOP_DIR%\mps-buildtools
-) else (
-    if exist %PROJECT_TOP_DIR%\..\mps-buildtools (
-       echo set  MPS_BUILDTOOLS=%PROJECT_TOP_DIR%\..\mps-buildtools
-       set  MPS_BUILDTOOLS=%PROJECT_TOP_DIR%\..\mps-buildtools
-        ) else (
-             if "%MPS_BUILDTOOLS%"=="" (
-                echo "Cannot find mps-buildtools"
-                exit /B
-           )
-     )
+echo "Getting MPS_BUILDTOOLS..."
+set blist= %MPS_BUILDTOOLS% %PROJECT_TOP_DIR% %PROJECT_TOP_DIR%\.. %HOMEPATH%\.local\.lib
+for %%b in (%blist%) do (
+  set bt=%%b%\mps-buildtools
+  echo "Look for %bt%"
+  if exist %bt% (
+    if exist %bt%\configure.py (
+      echo "Found %bt%"
+      goto :found_buildtools
+    )
+  )
 )
+echo "Cannot find mps-buildtools"
+exit /B
 
-echo "MPS_BUILDTOOLS %MPS_BUILDTOOLS%"
+:found_buildtools
+MPS_BUILDTOOLS=%bt%
 
-echo "Running configure.py ..."
+echo "Running configure.py..."
 %PYTHON%  %MPS_BUILDTOOLS%\configure.py %*
 
 echo "Configure done."
