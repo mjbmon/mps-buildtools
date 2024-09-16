e6d30086a3810c081c7f922a022f23f94cc4cc0d
Mark Sears <mjbmon@swcp.com>
2024-09-16 14:14:46 -0600

    New version of configure.bat for Windows to use list of paths to search for mps-buildtools

--- mps-buildtools/scripts/configure.bat
+++ mps-buildtools/scripts/configure.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 echo "Windows configure script"
 
@@ -14,24 +15,29 @@ FOR /F "delims=" %%i IN ('%P%') DO set PYTHON_VERSION=%%i
 echo "PYTHON VERSION is %PYTHON_VERSION%"
 
 echo "Getting MPS_BUILDTOOLS..."
-set blist= %MPS_BUILDTOOLS% %PROJECT_TOP_DIR% %PROJECT_TOP_DIR%\.. %HOMEPATH%\.local\.lib
+set blist=%PROJECT_TOP_DIR% %PROJECT_TOP_DIR%\.. %HOMEDRIVE%%HOMEPATH%\.local\lib
+echo "got here"
+echo "blist %blist%"
 for %%b in (%blist%) do (
   set bt=%%b%\mps-buildtools
-  echo "Look for %bt%"
-  if exist %bt% (
-    if exist %bt%\configure.py (
-      echo "Found %bt%"
+  echo "Look for !bt!"
+  if exist !bt! (
+    if exist !bt!\configure.py (
+      echo "Found !bt!"
       goto :found_buildtools
     )
   )
+
 )
+
 echo "Cannot find mps-buildtools"
 exit /B
 
 :found_buildtools
-MPS_BUILDTOOLS=%bt%
+echo "Found !bt!"
+set MPS_BUILDTOOLS=!bt!
 
 echo "Running configure.py..."
-%PYTHON%  %MPS_BUILDTOOLS%\configure.py %*
+%PYTHON%  !MPS_BUILDTOOLS!\configure.py %*
 
 echo "Configure done."
